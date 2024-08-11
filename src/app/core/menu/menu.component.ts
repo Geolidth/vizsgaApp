d2d8a4b45622c99b7f8efe51ce42fb5538c9cb2e
Geolidth <myyouz74@gmail.com>
2024-08-11 15:01:55 +0200

    First Initial Commit

---
+++ src/app/core/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss',
+})
+export class MenuComponent {
+  logo = signal('../assets/logo.jpg');
+}
